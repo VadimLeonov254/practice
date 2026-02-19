66b8a06bac8e04ef5b8d494ba95b8fe77db4f297
Vadim <vadleonov254@gmail.com>
2026-02-18 22:17:11 -0500

    dsa problems day 4

--- dsa/day_three/longest_sub_no_repeats/sub.cpp
+++ dsa/day_three/longest_sub_no_repeats/sub.cpp
@@ -1,56 +1,26 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<string>
-#include<map>
-#include<cmath>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
 
-int main(){
-    
-
-    std::cout << "Enter the word: ";
+int main() {
     std::string word;
+    std::cout << "Enter the word: ";
     std::cin >> word;
-    int maxi = 0; 
-    std::vector<int> nums;
-    
-    std::map<char, int> letters;
 
-    int len = 0;
-    
-    for(int i = 0; i < word.size(); i++){
-        if(letters.find(word[i]) == letters.end()){
-            letters.emplace(word[i], 1);
-        }else{
-            letters[word[i]]++;
+    std::unordered_map<char, int> lastSeen;
+    int maxLen = 0;
+
+    for (int l = 0, r = 0; r < word.size(); r++) {
+
+        if (lastSeen.count(word[r]) && lastSeen[word[r]] >= l) {
+            l = lastSeen[word[r]] + 1;
         }
+
+        lastSeen[word[r]] = r;
+        maxLen = std::max(maxLen, r - l + 1);
     }
 
-    for(int l = 0, r = word.size() - 1;;){
-        if(word[l] != word[r] && letters[word[l]] == 1 && letters[word[r]] == 1){
-            std::cout << std::abs(r-l+1) << '\n';
-            break;
-        }else if(word[l] != word[r] && letters[word[l]] > 1 && letters[word[r]] == 1){
-            letters[word[l]]--;
-            l++;
-            if(letters[word[l-1]] > 1){
-                r--;
-            }
-        }else if(word[l] != word[r] && letters[word[l]] == 1 && letters[word[r]] > 1){
-            letters[word[r]]--;
-            r--;
-            if(letters[word[r+1]] > 1){
-                l++;
-            }
-        }else if(word[l] == word[r]){
-            l++;
-            r--;
-        }else if(word[l] != word[r] && letters[word[l]] > 1 && letters[word[r]] > 1){
-            letters[word[l]]--;
-            letters[word[r]]--;
-            r--;
-            l++;
-        }
-    }
-
+    std::cout << maxLen << '\n';
 }
+
