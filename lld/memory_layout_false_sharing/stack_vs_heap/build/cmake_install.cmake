0736a4da88f4dd30801bb663679a72a770841378
VadimLeonov254 <vadleonov254@gmail.com>
2026-02-24 20:35:40 -0500

    memory layout and false sharing stack vs heap problem

--- lld/memory_layout_false_sharing/stack_vs_heap/build/cmake_install.cmake
+++ lld/memory_layout_false_sharing/stack_vs_heap/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/c/Users/vadle/practice/dsa/day_six/longest_repeating_replacement
+# Install script for directory: /mnt/c/Users/vadle/practice/lld/memory_layout_false_sharing/stack_vs_heap
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Users/vadle/practice/dsa/day_six/longest_repeating_replacement/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/c/Users/vadle/practice/lld/memory_layout_false_sharing/stack_vs_heap/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
